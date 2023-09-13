8bf4e30589848f65d1224e1c8d38e50702725eb6
Nicole T <nbtrieu@uci.edu>
2023-09-13 10:33:06 -0700

    Send user input data to the backend

--- src/components/GroupForm/CpGUploader.js
+++ src/components/GroupForm/CpGUploader.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Papa from 'papaparse';
+import axios from 'axios';
 
 const CpGUploader = () => {
   const [fileName, setFileName] = useState('');
@@ -16,36 +17,70 @@ const CpGUploader = () => {
 
   const handleNameChange = (event) => {
       setGroupName(event.target.value);
+      // console.log("Current groupName state:", groupName);
+  };
+
+  const fetchData = async () => {
+    try {
+        const formData = new FormData();
+        formData.append('cpgFile', file);
+        formData.append('groupName', groupName);
+
+        const response = await axios.post('http://localhost:5000/run-import-cpg-data', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        });
+
+        console.log(response.data);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+
+    // Initially clear any previous messages
+    setMessage('');
+
+    // Check if either file or groupName is missing -- BUG: not working for some reason..
+    if (!file || !groupName) {
+      console.log('>>> no file or no name provided');
+      setMessage('Please provide both a valid file and a group name.');
+      console.log("Current message state:", message);
+      return;  // Exit early
+  }
+
     try {
       if (file) {
         Papa.parse(file, {
-            complete: (result) => {
-                console.log('Parsed Result:', result);
-                const cpGs = result.data.map(row => row[0]); // assuming first column has CpG names
-                console.log('CpGs:', cpGs);                
-            },
-            header: false
-        });
-      }
+          complete: async (result) => {
+            console.log('Parsed Result:', result);
+            const cpGs = result.data.map(row => row[0]); // **PROVIDE TEMPLATE wherein FIRST column has the cpg names
+            console.log('CpGs:', cpGs);  
+            
+            // After parsing and possibly validating the file, send data to backend
+            await fetchData();
 
-      setMessage('Your CpG group has been generated.');
-      setTimeout(() => {
-        setMessage('');
-        setFileName('');
-        setFile(null);
-        setGroupName('');
-      }, 5000);
-    
+            // Once the file is successfully sent and processed, update the message and reset other states
+            setMessage('Your CpG group has been generated.');
+            setTimeout(() => {
+                setMessage('');
+                setFileName('');
+                setFile(null);
+                setGroupName('');
+            }, 5000);
+          },
+          header: false
+        });
+      } 
     } catch (error) {
       console.error('>>> handleFormSubmit error: ', error);
       setMessage(`Form was not submitted. ${error.message}`)
     }
-    
   };
+
 
   return (
       <div className='form form-wrapper mx-4 my-3'>
@@ -80,7 +115,7 @@ const CpGUploader = () => {
           />
         </div>
         <div className='my-3'>
-          <button onClick={handleSubmit}>Submit</button>
+          <button onClick={handleSubmit} type="submit" disabled={!file || !groupName}>Submit</button>
         </div>
         <div className='mb-3 message'>
           <p>{message}</p>
